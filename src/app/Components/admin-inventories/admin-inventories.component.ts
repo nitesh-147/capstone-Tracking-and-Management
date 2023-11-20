4c0d94647d0f045b24d8c6e431b53ec45b7dca3d
Nitesh Kumar Mehta <niteshkumarmehta364@gmail.com>
2023-11-20 06:58:38 +0530

    updated inventory part

--- src/app/Components/admin-inventories/admin-inventories.component.ts
+++ src/app/Components/admin-inventories/admin-inventories.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IInventory } from 'src/app/Models/inventory';
+import { InventoryServiceService } from 'src/app/Services/inventory-service.service';
 
 @Component({
   selector: 'app-admin-inventories',
   templateUrl: './admin-inventories.component.html',
   styleUrls: ['./admin-inventories.component.css']
 })
-export class AdminInventoriesComponent {
+export class AdminInventoriesComponent implements OnInit {
+  inventoriesList: IInventory[] = [];
+  constructor(
+    private inventoryService: InventoryServiceService
+  ) { }
 
+  ngOnInit(): void {
+    this.fetchInventory();
+  }
+
+  fetchInventory() {
+    this.inventoryService.getInventories().subscribe((res) => {
+      this.inventoriesList = res;
+    });
+  }
 }
